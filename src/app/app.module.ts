0cee77cc5e0d8143e333caf08604431ac7832fd6
adrwlo <adrwlo@o2.pl>
2024-01-16 14:37:46 +0100

    added necessary modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UpdateUserComponent } from './modules/update-user/update-user.component';
+import { AddUserComponent } from './modules/add-user/add-user.component';
+import { ListUsersComponent } from './modules/list-users/list-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpdateUserComponent,
+    AddUserComponent,
+    ListUsersComponent
   ],
   imports: [
     BrowserModule,
